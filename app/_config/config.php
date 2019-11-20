1ab2d3f4f31988a26665d406308ec071e52f7be4
Luis Cruz <luis@kafeina.com>
2019-11-19 21:17:48 -0600

    Update

--- app/_config/config.php
+++ app/_config/config.php
@@ -4,7 +4,7 @@ switch($_SERVER['SERVER_NAME']){
   case "localhost":
   define("SERVER_HOST","localhost");
   define('SERVER_USER',"root");
-  define('SERVER_PASS',"root");
+  define('SERVER_PASS',"qwerty123");
   define('SERVER_DB',"eguerra");
   break;
 
